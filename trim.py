4197b67522104d04a5df75fc6cd2355428d3e6a1
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-11-17 21:44:46 -0500

    changes

--- trim.py
+++ trim.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
 df_data = pd.read_csv("WeatherEvents_Jan2016-Dec2021.csv")
+df_data = pd.read_csv("Weather_Events.csv")
 
 print(df_data.columns)
 
 df_data = df_data.drop(df_data.columns[[0,5,6,7,8,9,10,13]], axis=1)
 
+
+print(df_data.columns)
+print(df_data.head())
+df_data = df_data.drop(df_data.columns[[0,2,5,6,7,8,9,10,13]], axis=1)
 
 print(df_data.columns)
 print(df_data.head())
