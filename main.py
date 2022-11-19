6e935c1a0999bc7378f0fa98b2344174d878e589
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-11-19 14:52:03 -0500

    Added code

--- main.py
+++ main.py
@@ -15,5 +15,15 @@ def generate_graph(df, xAxis, yAxis, graph_title, graph_color):
 
 
 
-# Read in accident data pickle
-accident_data = pd.read_pickle("US_Accidents.pkl")
+# Read in accident data
+acc_data = pd.read_csv("US_Accidents_Dec21_updated.csv")
+
+print(acc_data.columns)
+
+acc_data = acc_data.drop(acc_data.columns[[0,1,3,4,5,6,7,8,9,10,11,12,13,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]], axis=1)
+
+print(acc_data.columns)
+print(acc_data)
+
+#df_data = df_data.loc[df_data['Defining Parameter'] == '']
+
