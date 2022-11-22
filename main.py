09d447076e997a843a5991ad6f5a70b6252c364c
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-11-21 20:02:31 -0500

    Additional code

--- main.py
+++ main.py
@@ -16,14 +16,55 @@ def generate_graph(df, xAxis, yAxis, graph_title, graph_color):
 
 
 # Read in accident data
-acc_data = pd.read_csv("US_Accidents_Dec21_updated.csv")
+acc_data = pd.read_csv('US_Accidents_Dec21_updated.csv')
 
 print(acc_data.columns)
 
-acc_data = acc_data.drop(acc_data.columns[[0,1,3,4,5,6,7,8,9,10,11,12,13,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]], axis=1)
+acc_data = acc_data.drop(acc_data.columns[[0,3,4,5,6,7,8,9,10,11,12,13,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46]], axis=1)
+
+acc_data['State, County'] = acc_data['State'].astype(str) + ", " + acc_data['County'].astype(str)
+acc_data = acc_data.drop(acc_data.columns[[2,3]], axis=1)
+
+location_list = acc_data['State, County'].unique()
+
+
+grouped_acc_data = acc_data.groupby(['State, County'])
+
+for location in grouped_acc_data:
+    
+
+
+#count = 0
+#grouped_acc_dfs = {}
+#for item in location_list:
+#    location = location_list[count]
+#    
+#    df = acc_data.groupby(location)
+#    
+#    grouped_acc_dfs[location] = df
+#    
+#    count = count + 1
+#
+#grouped_acc_dfs = {}
+#
+#while count < len(location_list):
+#    # dynamically create key
+#    key = ...
+#    # calculate value
+#    value = ...
+#    a[key] = value 
+#    k += 1
+
+
+
+for row in acc_data.head().itertuples():
+    print(row.Index, row.Severity, row.Start_Time)
+    
+#print(location_list)
+#print(location_list.dtype)
+
 
 print(acc_data.columns)
-print(acc_data)
 
 #df_data = df_data.loc[df_data['Defining Parameter'] == '']
 
--- trim.py
+++ trim.py
@@ -4,10 +4,22 @@ df_data = pd.read_csv("WeatherEvents_Jan2016-Dec2021.csv")
 
 print(df_data.columns)
 
-df_data = df_data.drop(df_data.columns[[0,2,5,6,7,8,9,10,13]], axis=1)
+df_data = df_data.drop(df_data.columns[[0,5,6,7,8,9,10,13]], axis=1)
+
 
 print(df_data.columns)
 print(df_data.head())
 
 #df_data = df_data.loc[df_data['Defining Parameter'] == '']
 
+df_data['StartTime(UTC)'] = pd.to_datetime(df_data['StartTime(UTC)'])
+df_data['EndTime(UTC)'] = pd.to_datetime(df_data['EndTime(UTC)'])
+
+df_data.rename(columns={'StartTime(UTC)': 'Start_Time', 'EndTime(UTC)': 'End_Time'}, inplace=True)
+
+print(df_data.columns)
+print(df_data.head())
+
+
+
+#print(df_data.dtypes)
