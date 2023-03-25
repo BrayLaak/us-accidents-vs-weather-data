7ec258c89a73f1ee7a020e9af253ecf96b04cfad
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2023-03-25 15:09:08 -0400

    Adding code for multiple fixes and the join

--- main.py
+++ main.py
@@ -11,50 +11,79 @@ def generate_graph(df, xAxis, yAxis, graph_title, graph_color):
     df_graph.figure.savefig(graph_title + '.pdf')
     return
 
-# Function to convert a datetime to UTC based on the timezone
-def convert_to_utc(dt, tz):
-    local_tz = pytz.timezone(tz)
-    local_time = local_tz.localize(dt, is_dst=None)
-    utc_time = local_time.astimezone(pytz.utc)
-    return utc_time
 
 # Function to calculate the distance between two points
 def calculate_distance(lat1, lon1, lat2, lon2):
     return great_circle((lat1, lon1), (lat2, lon2)).miles
 
+# Function to perform the cross join on dataframes for a state
+def cross_join_state(acc_data, weather_data, threshold_distance):
+    result_dataframes = []
+    states = acc_data['State'].unique()
+
+    for state in states:
+        acc_df = acc_data[acc_data['State'] == state]
+        weather_df = weather_data[weather_data['State'] == state]
+
+        # Sorts dataframes by time for merge_asof
+        acc_df = acc_df.sort_values('Start_Time_UTC')
+        weather_df = weather_df.sort_values('StartTime(UTC)')
+
+        # Performs asof merge on sorted dataframes
+        merged_df = pd.merge_asof(acc_df, weather_df, left_on='Start_Time_UTC', right_on='StartTime(UTC)', direction='forward', suffixes=('_acc', '_weather'))
+
+        # Calculates distance for each row
+        merged_df['distance'] = merged_df.apply(lambda row: calculate_distance(row['Start_Lat'], row['Start_Lng'], row['LocationLat'], row['LocationLng']), axis=1)
+
+        # Filters rows based on distance threshold
+        merged_df = merged_df[merged_df['distance'] <= threshold_distance]
+
+        result_dataframes.append(merged_df)
+
+    return pd.concat(result_dataframes).reset_index(drop=True)
+
+
+
 # Defines a threshold distance in miles
 threshold_distance = 5
 
 
-# Read in accident data
+# Reads in accident data
 acc_data = pd.read_csv("US_Accidents.csv")
 
-# Remove NAN values from acc_data
+# Removes NAN values from acc_data
 acc_data = acc_data.dropna()
 
+# Selects the columns that are needed for the analysis
+acc_data = acc_data[['ID', 'Severity', 'State', 'Start_Time', 'Start_Lat', 'Start_Lng', 'Timezone']]
 
-#print(acc_data.columns)
-
-acc_data = acc_data[['ID', 'Severity', 'Start_Time', 'Start_Lat', 'Start_Lng', 'Timezone']]
+acc_data = acc_data.rename(columns={'Severity': 'Acc_Severity'})
 
 # Ensures the 'Start_Time' column is in datetime format
 acc_data['Start_Time'] = pd.to_datetime(acc_data['Start_Time'])
 
-# Applies the conversion function to the Start_Time column
-acc_data['Start_Time_UTC'] = acc_data.apply(lambda row: convert_to_utc(row['Start_Time'], row['Timezone']), axis=1)
+# Converts the timezone of the 'Start_Time' column and renames the column to 'Start_Time_UTC'
+acc_data['Start_Time'] = acc_data['Start_Time'].dt.tz_localize(acc_data['Timezone']).dt.tz_convert('UTC')
+acc_data.rename(columns={'Start_Time': 'Start_Time_UTC'}, inplace=True)
 
-# Drops the original Start_Time column and Timezone column
-acc_data = acc_data.drop(['Start_Time', 'Timezone'], axis=1)
-
-
-
+# Drops the Timezone column
+acc_data = acc_data.drop(['Timezone'], axis=1)
 
 print(acc_data.head())
+
+
 
 #read in weather data
 weather_data = pd.read_csv("Weather_Events.csv")
 
-weather_data = weather_data[['EventId', 'Type', 'Severity', 'StartTime(UTC)', 'EndTime(UTC)', 'Precipitation(in)', 'LocationLat', 'LocationLng']]
+# Select the columns that are needed for the analysis
+weather_data = weather_data[['Type', 'State', 'Severity', 'StartTime(UTC)', 'EndTime(UTC)', 'Precipitation(in)', 'LocationLat', 'LocationLng']]
+
+# Rename the 'Severity' column to 'Weather_Severity' to avoid confusion with the 'Severity' column from the accident data
+weather_data = weather_data.rename(columns={'Severity': 'Weather_Severity'})
+
+# Rename the 'Type' column to 'Weather_Type'
+weather_data = weather_data.rename(columns={'Type': 'Weather_Type'})
 
 # drop NAN values from weather_data
 weather_data = weather_data.dropna()
@@ -63,45 +92,56 @@ weather_data = weather_data.dropna()
 weather_data['StartTime(UTC)'] = pd.to_datetime(weather_data['StartTime(UTC)'])
 weather_data['EndTime(UTC)'] = pd.to_datetime(weather_data['EndTime(UTC)'])
 
+# Ensures the 'StartTime(UTC)' and 'EndTime(UTC)' columns are set to UTC timezone
+weather_data['StartTime(UTC)'] = weather_data['StartTime(UTC)'].dt.tz_localize('UTC')
+weather_data['EndTime(UTC)'] = weather_data['EndTime(UTC)'].dt.tz_localize('UTC')
 
 
 print(weather_data.head())
 
 
+# Group the dataframes by state
+acc_data_groups = acc_data.groupby('State')
+weather_data_groups = weather_data.groupby('State')
+
+# Get unique states from both dataframes
+unique_states = set(acc_data['State'].unique()).union(weather_data['State'].unique())
+
+# Initialize a list to store the dataframes for each state
+result_dataframes = []
+
+# Iterate through the unique states and perform the cross join for each state
+for state in unique_states:
+    acc_df = acc_data[acc_data['State'] == state]
+    weather_df = weather_data[weather_data['State'] == state]
+
+    merged_df = cross_join_state(acc_df, weather_df, threshold_distance)
+
+    # Append the merged_df to the list of dataframes
+    result_dataframes.append(merged_df)
+
+# Concatenate the list of dataframes into a single dataframe
+result_df = pd.concat(result_dataframes)
+
+# Reset the index of the final dataframe
+result_df.reset_index(drop=True, inplace=True)
+
+print(result_df.head())
+
 '''
+# Generate a graph for the number of accidents per weather type
+accidents_per_weather_type = result_df.groupby('Weather_Type').size().reset_index(name='Count')
+generate_graph(accidents_per_weather_type, 'Weather_Type', 'Count', 'Accidents per Weather Type', 'red')
 
-# Merge dataframes based on conditional statement
-merged_df = pd.merge(df1, df2[(df1['time'] <= df2['stop_time']) & (df1['time'] >= df2['start_time'])], 
-                     how='inner', left_index=True, right_index=True)
+# Generate a graph for the number of accidents per weather severity
+accidents_per_weather_severity = result_df.groupby('Weather_Severity').size().reset_index(name='Count')
+generate_graph(accidents_per_weather_severity, 'Weather_Severity', 'Count', 'Accidents per Weather Severity', 'blue')
 
-# Print merged dataframe
-print(merged_df)
-'''
+# Generate a graph for the number of accidents per weather type by year
+accidents_per_weather_type_by_year = result_df.groupby(['Weather_Type', result_df['Start_Time_UTC'].dt.year]).size().reset_index(name='Count')
+generate_graph(accidents_per_weather_type_by_year, 'Weather_Type', 'Count', 'Accidents per Weather Type by Year', 'red')
 
-'''
-# Define a function to calculate the distance between two points
-def calculate_distance(lat1, lon1, lat2, lon2):
-    return great_circle((lat1, lon1), (lat2, lon2)).miles
-
-# Define a threshold distance in miles (e.g., 5 miles)
-threshold_distance = 5
-
-# Perform a cross join on the dataframes
-df1['key'] = 1
-df2['key'] = 1
-merged_df = pd.merge(df1, df2, on='key').drop('key', axis=1)
-
-# Calculate the distance between each pair of points
-merged_df['distance'] = merged_df.apply(lambda row: calculate_distance(row['latitude_x'], row['longitude_x'], row['latitude_y'], row['longitude_y']), axis=1)
-
-# Filter the merged dataframe based on the threshold distance and the start time conditions
-merged_df = merged_df[(merged_df['distance'] <= threshold_distance) &
-                      (merged_df['start_time_x'] >= merged_df['start_time_y']) &
-                      (merged_df['start_time_x'] <= merged_df['end_time'])]
-
-# Clean up the dataframe by dropping unnecessary columns and resetting the index
-merged_df = merged_df.drop(['latitude_y', 'longitude_y', 'distance', 'start_time_y', 'end_time'], axis=1).reset_index(drop=True)
-merged_df.columns = ['A', 'start_time', 'latitude', 'longitude', 'B']
-
-print(merged_df)
+# Generate a graph for the number of accidents per weather severity by year
+accidents_per_weather_severity_by_year = result_df.groupby(['Weather_Severity', result_df['Start_Time_UTC'].dt.year]).size().reset_index(name='Count')
+generate_graph(accidents_per_weather_severity_by_year, 'Weather_Severity', 'Count', 'Accidents per Weather Severity by Year', 'blue')
 '''
